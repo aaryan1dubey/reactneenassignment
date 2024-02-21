50d408aa579c4f309011a462f63fed94ce62b76c
aaryan1dubey <73747549+aaryan1dubey@users.noreply.github.com>
2024-02-21 10:09:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import UserListing from "./components/UserListing";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <UserListing />
+    </div>
+  );
+}
+
+export default App;
