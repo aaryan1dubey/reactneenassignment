703ce2c837423ab8d4aa1f319cb89f05393c11f8
aaryan1dubey <73747549+aaryan1dubey@users.noreply.github.com>
2024-02-21 10:16:00 +0530

    Create UserData.js

---
+++ src/JsonData/UserData.js
@@ -0,0 +1,112 @@
+export const usersData = [
+  {
+    id: 1,
+    name: "Aaryan Dubey",
+    email: "Aaryan@gmail.com",
+    mobile: "9999999999",
+    website: "https://Aaryan.fyi",
+    company: "Sourcesoft Solutions",
+    address: "Sector 67, Noida",
+    image:
+      "https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+  },
+  {
+    id: 2,
+    name: "Jahnvi Shah",
+    email: "jahnvi@gmail.com",
+    mobile: "8888888888",
+    website: "https://jahnvi.com",
+    company: "Sourcesoft Solutions",
+    address: "Sector 67, Noida",
+    image:
+      "https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+  },
+  {
+    id: 3,
+    name: "Nikita Shiwach",
+    email: "nikita@gmail.com",
+    mobile: "7777777777",
+    website: "https://nikita.com",
+    company: "Sourcesoft Solutions",
+    address: "Sector 67, Noida",
+    image:
+      "https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+  },
+  {
+    id: 4,
+    name: "Dev Kumar",
+    email: "dec@gmail.com",
+    mobile: "6666666666",
+    website: "https://dev.com",
+    company: "Sourcesoft Solutions",
+    address: "Sector 67, Noida",
+    image:
+      "https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+  },
+  {
+    id: 5,
+    name: "Sumit Rout",
+    email: "sumit@gmail.com",
+    mobile: "5555555555",
+    website: "https://sumit.com",
+    company: "Sourcesoft Solutions",
+    address: "Sector 67, Noida",
+    image:
+      "https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+  },
+  {
+    id: 6,
+    name: "Ashish Belwal",
+    email: "ashish@gmail.com",
+    mobile: "4444444444",
+    website: "https://ashish.com",
+    company: "Sourcesoft Solutions",
+    address: "Sector 67, Noida",
+    image:
+      "https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+  },
+  {
+    id: 7,
+    name: "Ankit Pal",
+    email: "ankit@gmail.com",
+    mobile: "3333333333",
+    website: "https://ankit.com",
+    company: "Sourcesoft Solutions",
+    address: "Sector 67, Noida",
+    image:
+      "https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+  },
+  {
+    id: 8,
+    name: "Hariom Kumar",
+    email: "harion@gmail.com",
+    mobile: "2222222222",
+    website: "https://hariom.com",
+    company: "Sourcesoft Solutions",
+    address: "Sector 67, Noida",
+    image:
+      "https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+  },
+  {
+    id: 9,
+    name: "Hare Krishna",
+    email: "harekrishna@gmail.com",
+    mobile: "1111111111",
+    website: "https://harekrishna.com",
+    company: "Sourcesoft Solutions",
+    address: "Sector 67, Noida",
+    image:
+      "https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+  },
+  {
+    id: 10,
+    name: "Vijay Singh",
+    email: "vijay@gmail.com",
+    mobile: "4545568978",
+    website: "https://vijay.com",
+    company: "Sourcesoft Solutions",
+    address: "Sector 67, Noida",
+    image:
+      "https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+  },
+];
